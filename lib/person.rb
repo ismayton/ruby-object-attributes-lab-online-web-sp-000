c7c4f25f1938b53b3af320f8396e394595e1416e
Ian Mayton <ian.mayton@gmail.com>
2020-07-21 17:12:28 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,19 @@
 class Person
   
+  def name=(name)
+    @this_persons_name = name 
+  end
+  
+  def name 
+    @this_persons_name 
+  end
+  
+  def job=(job) 
+    @this_persons_job = job
+  end
+  
+  def job
+    @this_persons_job
+  end
+  
 end
